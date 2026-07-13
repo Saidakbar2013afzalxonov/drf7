a3ed0c5edb71efa18c85e9a64e93494b0b656367
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-07-13 15:37:07 +0500

    first_commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100,blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_year = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    ``
